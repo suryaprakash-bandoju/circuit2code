78cc2540088505cbb9b3675030dd5f8a73658b3a
suryaprakash-bandoju <bandojusuryaprakash3528@gmail.com>
2026-07-06 22:07:44 +0530

    Fixed all paths for GitHub Pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
     
-    // 1. Get the base path of the current page
+    // Get the base path of the current page
     // If the path is '/circuit2code/python/index.html', base will be '/circuit2code/'
     const pathArray = window.location.pathname.split('/');
     const basePath = '/' + pathArray[1] + '/'; 
 
-    // 2. Load the components using the calculated base path
+    // Load the components using the calculated base path
     const loadComponent = (path, elementId) => {
         fetch(basePath + path)
             .then(response => {
